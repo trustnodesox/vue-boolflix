bdfd7d49223cae44fa17e9de5bc4fcc0b879e4ff
Lorenzo De Sossi <lollodesossi@gmail.com>
2021-04-14 23:00:14 +0200

    completo senza opzionali

--- main.js
+++ main.js
@@ -3,55 +3,57 @@ Vue.config.devtools = true;
 var app = new Vue({
   el: '#root',
   data: {
-    risultati: ["..."],
+    risultati: [],
     api_key: 'bd0d6cf8c8039f80c4927c83d9525715',
     lang: 'it',
     uri: 'https://api.themoviedb.org/3',
     query: '',
-    placeholder: 'https://img.icons8.com/ios/452/image-not-avialable.png',
-    hover: false
-  },
-  mounted(){
-    //axios.get(`${this.poster}/w342?api_key=${this.api_key}`)
-    //.then((response) => {
-    //  console.log(response);
-    //})
-    axios.get(`${this.uri}/search/movie?api_key=${this.api_key}`)
-    .then((response) => {
-      console.log(response.data.results.backdrop_path);
-    })
-  },
-  methods: {
-    search: function () {
-    axios.get(`${this.uri}/search/movie?api_key=${this.api_key}&query=${this.query}&language=${this.lang}`)
-    .then((response) => {
-      this.risultati = [...this.risultati, ...response.data.results]
-      if (!this.risultati.poster.path) {
-      this.risultati.poster_path = this.placeholder
-      }
-    })
-    axios.get(`${this.uri}/search/tv?api_key=${this.api_key}&query=${this.query}&language=${this.lang}`)
-    .then((response) => {
-      this.risultati = [...this.risultati, ...response.data.results]
-    })
-  },
-  clear: function() {
-    this.risultati = []
-  },
-  GetTitle: function(obj){
-    if (obj.title) {
-      return obj.title;
-    } else {
-      return obj.name;
-    }
-  },
-  GetOriginaltitle: function(obj){
-    if (obj.original_title) {
-      return obj.original_title;
-    } else {
-      return obj.original_name;
-    }
+    placeholder: 'https://www.associazioneostetriche.it/wp-content/uploads/2018/05/immagine-non-disponibile.png',
+    show: true
   },
 
-}
-  })
+  methods: {
+    search: function () {
+      this.show = false;
+      this.risultati = [];
+      axios.get(`${this.uri}/search/movie?api_key=${this.api_key}&query=${this.query}&language=${this.lang}`)
+      .then((response) => {
+        this.risultati = [...this.risultati, ...response.data.results]
+      });
+      axios.get(`${this.uri}/search/tv?api_key=${this.api_key}&query=${this.query}&language=${this.lang}`)
+      .then((response) => {
+        this.risultati = [...this.risultati, ...response.data.results]
+      });
+    },
+
+    clear: function() {
+      this.risultati = []
+    },
+    GetTitle: function(obj){
+      if (obj.title) {
+        return obj.title;
+      } else {
+        return obj.name;
+      }
+    },
+    GetOriginaltitle: function(obj){
+      if (obj.original_title) {
+        return obj.original_title;
+      } else {
+        return obj.original_name;
+      }
+    },
+    votoStelle: function (vote) {
+     let stellapiena = '';
+     let stellavuota = '';
+     for (var i = 1; i <= 5; i++) {
+       if (i <= Math.round(vote / 2)) {
+         stellapiena += '<i class="fas fa-star"></i>';
+       } else {
+         stellavuota += '<i class="far fa-star"></i>';
+       }
+     }
+     return `${stellapiena}${stellavuota}`;
+    },
+  }
+})
